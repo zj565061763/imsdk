0fbd6591737e7c867e7ab634ac5322252bc0ce32
zjun <565061763@qq.com>
2020-03-23 10:07:28 +0800

    IMMessagePersistence

--- lib/src/main/java/com/sd/lib/imsdk/handler/impl/IMMessagePersistenceEmpty.java
+++ lib/src/main/java/com/sd/lib/imsdk/handler/impl/IMMessagePersistenceEmpty.java
@@ -1,5 +1,6 @@
 package com.sd.lib.imsdk.handler.impl;
 
+import com.sd.lib.imsdk.IMConversationType;
 import com.sd.lib.imsdk.IMMessage;
 import com.sd.lib.imsdk.IMMessageState;
 import com.sd.lib.imsdk.handler.IMMessagePersistence;
@@ -7,13 +8,13 @@ import com.sd.lib.imsdk.handler.IMMessagePersistence;
 public class IMMessagePersistenceEmpty implements IMMessagePersistence
 {
     @Override
-    public void saveMessage(IMMessage message)
+    public void saveMessage(IMMessage message, IMConversationType conversationType)
     {
 
     }
 
     @Override
-    public void updateMessageState(String id, IMMessageState state)
+    public void updateMessageState(String id, IMMessageState state, IMConversationType conversationType)
     {
 
     }
