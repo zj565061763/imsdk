06266baaeded7c058af9517d40efefb4007e2acb
zjun <565061763@qq.com>
2020-03-29 14:52:37 +0800

    IMConversationType

--- lib/src/main/java/com/sd/lib/imsdk/IMConversationType.java
+++ lib/src/main/java/com/sd/lib/imsdk/IMConversationType.java
@@ -4,4 +4,15 @@ public enum IMConversationType
 {
     single,
     group;
+
+    public static IMConversationType from(String name)
+    {
+        try
+        {
+            return valueOf(name);
+        } catch (Exception e)
+        {
+            return null;
+        }
+    }
 }
